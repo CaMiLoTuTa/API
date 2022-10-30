ccae48f081faba0a9fd655455b34b3b1036d49b7
CaMiLo TuTa <camilotuta2004@gmail.com>
2022-10-30 14:06:09 -0500

    Third Commit

--- oled.py
+++ oled.py
@@ -1,26 +1,33 @@
+
+# ? IMPORTAR LIBRERÍAS NECESARIAS
 import network
 import time
 import urequests
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 
+# ? INSTANCIA PANTALLA OLED
 i2c = I2C(0, scl=Pin(27), sda=Pin(14))
 ancho, alto = 128, 64
 oled = SSD1306_I2C(ancho, alto, i2c)
 
+# ? INSTANCIA BOTONES
 botonRojo = Pin(32, Pin.IN, Pin.PULL_UP)
 botonNegro = Pin(33, Pin.IN, Pin.PULL_UP)
 botonBlanco = Pin(26, Pin.IN, Pin.PULL_UP)
 
+#? URLS PARA ENVIAR INFORMACIÓN Y RECIBIR
 url = "https://maker.ifttt.com/trigger/notamb/with/key/inSrLr4nyMQ8ShQ0noTQEo4Hy2fO1f4ZYMVvxUEUW13"
 urlDis = "https://maker.ifttt.com/trigger/ambnot/with/key/inSrLr4nyMQ8ShQ0noTQEo4Hy2fO1f4ZYMVvxUEUW13?"
 urlSMS = "https://maker.ifttt.com/trigger/sms/with/key/inSrLr4nyMQ8ShQ0noTQEo4Hy2fO1f4ZYMVvxUEUW13?"
 urlTel = "https://maker.ifttt.com/trigger/telegram_not/with/key/inSrLr4nyMQ8ShQ0noTQEo4Hy2fO1f4ZYMVvxUEUW13?"
 
+# ? CONEXIÓN A LA RED
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 sta_if.connect("Q60", "minumero")
 
+# ? PANTALLA DE ESPERA MIENTRAS CONECTA
 while not sta_if.isconnected():
     oled.fill(0)
     oled.text("*"*16, 0, 0)
@@ -43,6 +50,7 @@ while not sta_if.isconnected():
     oled.show()
     time.sleep(0.5)
 
+# ? PANTALLA CUANDO ESTE CONECTADO A LA RED
 oled.fill(0)
 oled.text("*"*16, 0, 0)
 oled.text("   CONNECTED", 0, 30)
@@ -92,7 +100,7 @@ oled.text("*"*16, 0, 55)
 oled.show()
 time.sleep(0.5)
 
-
+# ? PANTALLA PRINCIPAL
 def presionar():
     oled.fill(0)
     oled.text("*"*16, 0, 0)
@@ -104,14 +112,13 @@ def presionar():
     oled.show()
     time.sleep(2)
 
-
+# ? DEPENDE DEL BOTON PRESIONADO MOSTRARA LA INFORMACIÓN
 def temperaturaOled():
     oled.fill(0)
     oled.text("*"*16, 0, 0)
     oled.text("TEMPERATURA: "+str(temperatura)+"C", 0, 30)
     oled.text("*"*16, 0, 57)
     oled.show()
-
 
 def humedadOled():
     oled.fill(0)
@@ -120,7 +127,6 @@ def humedadOled():
     oled.text("*"*16, 0, 57)
     oled.show()
 
-
 def presionOled():
     oled.fill(0)
     oled.text("*"*16, 0, 0)
@@ -128,7 +134,7 @@ def presionOled():
     oled.text("*"*16, 0, 57)
     oled.show()
 
-
+# ? ENVÍA LOS DATOS RECIBIDOS A LOS SERVICIOS DE IFTTT Y A LA PANTALLA OLED
 def enviarMensaje():
     if botonRojo.value() == 1 and botonNegro.value() == 1 and botonBlanco.value() == 1:
         presionar()
@@ -167,18 +173,21 @@ def enviarMensaje():
         presionOled()
 
 
+# ? CICLO PRINCIPAL
 while True:
-
+    # ~ VERIFICA EL VALOR DE LOS BOTONES Y MOSTRARA EL VALOR A PEDIR 
     if botonRojo.value() == 1 and botonNegro.value() == 1 and botonBlanco.value() == 1:
         presionar()
+    # ~ RECIBE LOS DATOS 
     consulta = urequests.get(
         "https://api.thingspeak.com/channels/1909834/feeds.json?results=2")
     datos = consulta.json()
-
+    # ~ VARIABLES DONDE SE GUARDARAN LOS DATOS DEL ARCHIVO JSON
     temperatura = datos["feeds"][1]["field1"]
     humedad = datos["feeds"][1]["field2"]
     presion = datos["feeds"][1]["field3"]
 
+    # ~ VERIFICA EL VALOR DE LOS BOTONES Y MOSTRARA EL VALOR A PEDIR 
     if botonRojo.value() == 0 and botonNegro.value() == 1 and botonBlanco.value() == 1:
         temperaturaOled()
     if botonRojo.value() == 1 and botonNegro.value() == 0 and botonBlanco.value() == 1:
@@ -186,12 +195,4 @@ while True:
     if botonRojo.value() == 1 and botonNegro.value() == 1 and botonBlanco.value() == 0:
         presionOled()
 
-    if botonRojo.value() == 1 and botonNegro.value() == 1 and botonBlanco.value() == 1:
-        presionar()
     enviarMensaje()
-    if botonRojo.value() == 0 and botonNegro.value() == 1 and botonBlanco.value() == 1:
-        temperaturaOled()
-    if botonRojo.value() == 1 and botonNegro.value() == 0 and botonBlanco.value() == 1:
-        humedadOled()
-    if botonRojo.value() == 1 and botonNegro.value() == 1 and botonBlanco.value() == 0:
-        presionOled()
--- sensor.py
+++ sensor.py
@@ -1,3 +1,5 @@
+
+# ? IMPORTAR LIBRERÍAS NECESARIASimport network
 import network
 import urequests
 import time
@@ -5,8 +7,10 @@ import random
 from dht import DHT11
 from machine import Pin as pin
 
+# ? INSTANCIA SENSOR DE AMBIENTE
 sensor = DHT11(pin(15))
 
+# ? CONEXIÓN A LA RED
 def wifi(red, password):
     global miRed
     miRed = network.WLAN(network.STA_IF)
@@ -26,6 +30,7 @@ if wifi("Q60", "minumero"):
     print('Datos de la red (IP/netmask/gw/DNS):', miRed.ifconfig())
     url = "https://api.thingspeak.com/update?api_key=N7L1OOYI6Y4IKQDL"
 
+# ? CICLO PRINCIPAL
     while True:
         time.sleep(0.3)
         sensor.measure()
@@ -34,6 +39,7 @@ if wifi("Q60", "minumero"):
         presion = random.randint(559, 563)
         # print("T={:02d}°C, H={:02d}%, P={:02d}Torr".format(
         #     temperatura, humedad, presion))
+        # ~ ENVÍA LOS DATOS A THING-SPEAK
         respuesta = urequests.get(
             url+"&field1="+str(temperatura)+"&field2="+str(humedad)+"&field3="+str(presion))
         # print(respuesta.text)
